0202ceab53fc9aaf7c349aab21c6020e9e6fdbdb
Deepika-M991 <TharunDeepika.M@conduent.com>
2024-08-28 16:58:24 +0530

    JPA with crudRepo

---
+++ crudRepo/crudRepo/src/test/java/com/skillsoft/crudRepo/CrudRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillsoft.crudRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
