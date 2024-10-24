d84bf580e89d4f991e9a6c52b3ae937a9a110b4d
Tharun Deepika M <TharunDeepika.M@conduent.com>
2024-10-24 16:16:39 +0530

    queryscheduler updated

---
+++ javaEmailSender/javaEmailSender/src/test/java/com/assessment1/javaEmailSender/JavaEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment1.javaEmailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
